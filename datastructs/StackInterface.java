662ff2605fa01563e043e0d46a3e602830b0277a
Luis Angel <luis_angel1993julio@hotmail.com>
2013-10-02 17:15:45 -0600

    Delete StackInterface.java
    Fixed and moved to file interfaces

--- datastructs/StackInterface.java
+++
@@ -1,7 +0,0 @@
-package edu.itcr.startec.datastructs;
-
-public interface StackInterface<K> extends ListInterface<K>{
-    public boolean push(K pk);
-    public String top();
-    public boolean pop();
-}
