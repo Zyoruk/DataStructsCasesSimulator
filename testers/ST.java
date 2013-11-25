f0a6266fc650715d64edfd011b68792e5c8d178c
Daniel Jenkins <jeukel@gmail.com>
2013-11-25 15:42:03 -0600

    Pasing time

--- testers/ST.java
+++ testers/ST.java
@@ -8,8 +8,40 @@ public class ST {
 	 */
 	public static void main(String[] args) {
 		SplayTree<Integer> tree = new SplayTree<Integer>();
-		tree.
-
+			
+		tree.insert(5);
+		System.out.println(tree.getRoot().getData());
+		
+		tree.insert(4);			
+		System.out.println(tree.getRoot().getData());
+		
+		tree.insert(6);
+		System.out.println(tree.getRoot().getData());
+		
+		System.out.println("*****************");
+		
+		System.out.println(tree.getRoot().getRight().getData());
+		System.out.println(tree.getRoot().getRight().getRight().getData());
+		//System.out.println(tree.getRoot().getRight().getLeft().getData());
+		
+		//tree.describe();
+		
+		/*
+		
+		tree.insert(7);
+		System.out.println(tree.getRoot().getData());
+		
+		tree.insert(3);
+		System.out.println(tree.getRoot().getData());
+		
+		tree.insert(2);
+		System.out.println(tree.getRoot().getData());
+		
+		tree.insert(8);
+		System.out.println(tree.getRoot().getData());
+		
+		*/
+			
 	}
 
 }
